9254b8c7e9935853685f6c76799a1b178eba6ddd
Praneeth-C <113545042+Praneeth-C@users.noreply.github.com>
2023-02-08 14:01:13 -0500

    Add files via upload

--- pygame/click_game.py
+++ pygame/click_game.py
@@ -9,7 +9,25 @@ def draw():
     screen.fill(BACKGROUND_COLOR)
     cry_man.draw()
 
+def set_no_cry_man():
+    cry_man.image = 'no_cry_man'
+
+def set_cry_man():
+    cry_man.image = 'cry_man'
+    clock.schedule_unique(set_no_cry_man, 0.25)
+
+def set_happy_man():
+    cry_man.image = 'happy_man'
+    clock.schedule_unique(set_no_cry_man, 1.0)
+
 def update():
     cry_man.left = cry_man.left + 2
     if cry_man.left > WIDTH:
         cry_man.right = 0
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        set_happy_man()
+    else:
+        set_cry_man()
+
