b006d04a4df3fcfc38ca2007ab6db5019acd2024
Praneeth-C <113545042+Praneeth-C@users.noreply.github.com>
2023-02-16 13:28:46 -0500

    Update number_game.py

--- follow_the_number_final/number_game.py
+++ follow_the_number_final/number_game.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 
 
 WIDTH = 400
@@ -11,19 +11,19 @@ lines = []
 next_dot = 0
 
 for dot in range(0, 10):
-    actor = Actor("dot")
-    actor.pos = (random.randint(20, WIDTH - 20), random.randint(20, HEIGHT - 20))
+    actor = Actor("coin")
+    actor.pos = (randint(20, WIDTH - 20), randint(20, HEIGHT - 20))
     dots.append(actor)
 
 def draw():
     screen.fill("black")
     number = 1
     for dot in dots:
-        screen.draw.text(str(number), dot.pos[0], dot.pos[1] + 12)
+        screen.draw.text(str(number), (dot.pos[0], dot.pos[1] + 12))
         dot.draw()
         number = number + 1
-    for line in lines:
-        screen.draw.line(line[0], line[1], (100, 0, 0))
+    for my_line in lines:
+        screen.draw.line(my_line[0], my_line[1], (100, 0, 0))
 
 def on_mouse_down(pos):
     global next_dot
